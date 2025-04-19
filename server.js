e0d09b123e4357fa0433dfe2092915e02c6367a7
Yogesh Yadav <yy838381@gmail.com>
2025-04-19 16:48:38 +0530

    sjhfdsk

--- server.js
+++ server.js
@@ -1616,7 +1616,7 @@ function parseSingleMCQ(rawResponse) {
   };
 }
 
-// Start the server
+// Start the server for me
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
